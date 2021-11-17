329bd9296724e13dd7b5281d7d04f10a2cd6677b
Evan Quah <evanjquah@gmail.com>
2021-11-17 01:20:25 -0600

    Learned how to do basic api calls using the keys stored in an enviornemental variable

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 #.venv\scripts\activate
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
+import os
 import numpy as np
 import pandas as pd
 import requests as rq
-import os
+import alpaca_trade_api as tradeapi
 from dotenv import load_dotenv
+load_dotenv()
 
-load_dotenv
+key_id = os.getenv("APCA_API_KEY_ID")
+secret_id = os.getenv("APCA_API_SECRET_KEY")
+base_url = os.getenv("APCA_API_BASE_URL")
 
-API = os.getenv("TEST")
-print(API)
+api = tradeapi.REST(key_id=key_id,secret_key=secret_id,base_url=base_url,api_version='v2')
+account = api.get_account()
+print(account)
