729de4e728db0fd84492cee3dd2ef4a19377645a
Evan Quah <evanjquah@gmail.com>
2021-11-19 23:53:01 -0600

    re-formatted some of the varibles

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ import numpy as np
 import pandas as pd
 import requests as rq
 import alpaca_trade_api as tradeapi
+from alpaca_trade_api import REST, TimeFrame
 from dotenv import load_dotenv
 load_dotenv()
 
 key_id = os.getenv("APCA_API_KEY_ID")
 secret_id = os.getenv("APCA_API_SECRET_KEY")
 base_url = os.getenv("APCA_API_BASE_URL")
+data_url = os.getenv("APCA_API_DATA_URL")
 
-api = tradeapi.REST(key_id=key_id,secret_key=secret_id,base_url=base_url,api_version='v2')
-account = api.get_account()
-print(account)
+api = tradeapi.REST(
+    key_id=key_id,
+    secret_key=secret_id,
+    base_url=base_url,
+)
+spy_quote = api.get_latest_quote('SPY')
+print(spy_quote)
